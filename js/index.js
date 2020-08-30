fb834a292fda325bb8909a620d9c0b781a11cad8
websfacil <70266345+websfacil@users.noreply.github.com>
2020-08-30 14:31:40 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,204 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.lenght -1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const prevtDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide)
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevtDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex)
+});
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide)
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex)
+});
+
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex)
+});
+
+$(document).ready(function(){   
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    }); 
+});
+
+
+// Creare's 'Implied Consent' EU Cookie Law Banner v:2.4
+// Conceived by Robert Kent, James Bavington & Tom Foyster
+// Put into a namespace and by Bjørn Rosell
+//   Also changed behaviour so you have to click accept to make the banner stay away.
+//   To make it behave like the original, set "createCookieWhenBannerIsShown" to true.
+
+var CookieBanner = (function() {
+    return {
+        'createCookieWhenBannerIsShown': false,
+        'createCookieWhenAcceptIsClicked': true,
+        'cookieDuration': 14,                   // Number of days before the cookie expires, and the banner reappears
+        'cookieName': 'cookieConsent',          // Name of our cookie
+        'cookieValue': 'accepted',              // Value of cookie
+
+        '_createDiv': function(html) {
+            var bodytag = document.getElementsByTagName('body')[0];
+            var div = document.createElement('div');
+            div.setAttribute('id','cookie-law');
+            div.innerHTML = html;
+
+            // bodytag.appendChild(div); // Adds the Cookie Law Banner just before the closing </body> tag
+            // or
+            bodytag.insertBefore(div,bodytag.firstChild); // Adds the Cookie Law Banner just after the opening <body> tag
+
+            document.getElementsByTagName('body')[0].className+=' cookiebanner'; //Adds a class tothe <body> tag when the banner is visible
+
+            if (CookieBanner.createCookieWhenBannerIsShown) {
+                CookieBanner.createAcceptCookie();
+            }
+        },
+
+        '_createCookie': function(name, value, days) {
+            var expires;
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime()+(days*24*60*60*1000));
+                expires = "; expires="+date.toGMTString();
+            }
+            else {
+                expires = "";
+            }
+            document.cookie = name+"="+value+expires+"; path=/";
+        },
+
+        '_checkCookie': function(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        },
+
+        '_eraseCookie': function(name) {
+            CookieBanner._createCookie(name,"",-1);
+        },
+
+        'createAcceptCookie': function() {
+            CookieBanner._createCookie(CookieBanner.cookieName, CookieBanner.cookieValue, CookieBanner.cookieDuration); // Create the cookie
+        },
+
+        'closeBanner': function() {
+            var element = document.getElementById('cookie-law');
+            element.parentNode.removeChild(element);
+        },
+
+        'accept': function() {
+            CookieBanner.createAcceptCookie();
+            CookieBanner.closeBanner();
+        },
+
+        'showUnlessAccepted': function(html) {
+            //alert(CookieBanner._checkCookie(CookieBanner.cookieName));
+            //alert(document.cookie);
+            if(CookieBanner._checkCookie(CookieBanner.cookieName) != CookieBanner.cookieValue){
+                CookieBanner._createDiv(html);
+            }
+        }
+
+    }
+
+})();
+
+window.onload = function(){ alert('Working!!'); }
+
+window.onload = function(){
+    console.log("dasdasd")
+    //var html = '<p>Our website uses cookies. By continuing we assume your permission to deploy cookies, as detailed in our <a href="/privacy-cookies-policy/" rel="nofollow" title="Privacy &amp; Cookies Policy">privacy and cookies policy</a>. <a class="close-cookie-banner" href="javascript:void(0);" onclick="CookieBanner.accept();"><span>X</span></a></p>'
+    var html = '<div>' +
+        'This site uses cookies to enhance your experience, personalize content and advertisements, and ' +
+        'analyze traffic. We may share your information with our marketing and analytics partners. ' +
+        'For further information, please review our ' +
+        '<a href="">Privacy Policy</a>. ' +
+        'By clicking "Accept", you agree to our use of cookies and similar technologies.' +
+        '</div>'
+
+    // Add the accept button
+    html += '<div><a href="javascript:void(0);" onclick="CookieBanner.accept();"><span>Accept</span></a></div>';
+
+    CookieBanner.showUnlessAccepted(html);
+}
+
